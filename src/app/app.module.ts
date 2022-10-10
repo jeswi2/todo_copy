2e1e8f01f7e391d186689580729051fed49991e6
jeswi2 <jeswimathewm@gmail.com>
2022-10-10 21:25:26 +0530

    ro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TodolistComponent } from './todolist/todolist.component';
 
+const appRoutes:Routes=[
+  {
+    path:"",component:TodolistComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +20,8 @@ import { TodolistComponent } from './todolist/todolist.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
